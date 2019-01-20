8abe7663f2c4c0bfb54df964f8a705065a0a778e
sortai <32301806+sortai@users.noreply.github.com>
2019-01-20 10:59:26 +0100

    Added SizeMismatch exception and used it
    Added SizeMismatch exception and added size mismatch check in the ann class

--- ann.py
+++ ann.py
@@ -1,4 +1,7 @@
 
+class SizeMismatch (Exception):
+    pass
+
 class layer:
     def __init__(self, size):
         pass
@@ -7,3 +10,18 @@ class ann (layer):
     def __init__(self, size, layers=None):
         if layers is None:
             super().__init__(self, size)
+        else:
+            if len(layers) < 1:
+                raise SizeMismatch("not enough layers provided")
+            if size[0]!=layers[0].size[0]:
+                raise SizeMismatch("The input size of the network doesn't match with the input size of the first layer")
+            if size[1]!=layers[-1].size[2]:
+                raise SizeMismatch("The output size of the network doesn't match with the output size of the last layer")
+            self.size = size
+            self.layers = layers
+            self.sizes=[self.size[0]]
+            for ln, la in enumerate(self.layers[:-1]):
+                if la.size[1]!=self.layers[ln+1].size[0]:
+                    raise SizeMismatch("Layer output size of layer {ln0} ({so}) and layer input size of layer {ln1} ({si}) don't match".format(ln0=ln, ln1=ln+1, so=la.size[1], si=self.layers[ln+1].size[0]))
+                else: self.sizes.append(la.size[1])
+            self.sizes.append(self.size[1])
