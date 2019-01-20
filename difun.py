5c11db2dae7e483196562b40fd54ddeaceeb6b9e
sortai <32301806+sortai@users.noreply.github.com>
2019-01-20 16:56:55 +0100

    made p a property and simplified derivative

--- difun.py
+++ difun.py
@@ -4,14 +4,19 @@ class difun:
     def __init__(self, fun, funp=None, p=2**-10):
         self.__fun = fun
         self.__funp = funp
-        self.p = p
+        if self.__funp is not None: p = 0
+        self.__p=p
+    @property
+    def p(self):
+        return self.__p
+    @p.setter
+    def p(self, p):
+        if self.__funp is None: self.__p=p
     def __call__(self, i):
         return self.__fun(i)
     def de(self, i):
-        try: return self.__funp(i)
-        except TypeError as error:
-            if error.args[0] == "'NoneType' object is not callable":
-                return (self(i+self.p)-self(i))/self.p
+        if self.__funp is not None: return self.__funp(i)
+        return (self(i+self.__p)-self(i))/self.__p
 
 Dfuns = {
     "lReLU": difun(lambda x: max(x,x*.1)),
