0158198a6ff325383b4e2aa442b8f0c21faa9739
sortai <32301806+sortai@users.noreply.github.com>
2019-01-20 10:45:48 +0100

    created the "layer" class, made the ann class inherit from it

--- ann.py
+++ ann.py
@@ -1,2 +1,9 @@
-class ann:
-    pass
+
+class layer:
+    def __init__(self, size):
+        pass
+
+class ann (layer):
+    def __init__(self, size, layers=None):
+        if layers is None:
+            super().__init__(self, size)
