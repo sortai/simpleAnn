dc65e7ca9bb3016599ff810b9f4c6a5f4e25f5c6
sortai <32301806+sortai@users.noreply.github.com>
2019-01-20 16:50:29 +0100

    created Dfun
    also made a small change to the difun class

--- difun.py
+++ difun.py
@@ -4,11 +4,15 @@ class difun:
     def __init__(self, fun, funp=None, p=2**-10):
         self.__fun = fun
         self.__funp = funp
-        self.__p = p
+        self.p = p
     def __call__(self, i):
         return self.__fun(i)
     def de(self, i):
         try: return self.__funp(i)
         except TypeError as error:
             if error.args[0] == "'NoneType' object is not callable":
-                return (self(i+self.__p)-self(i))/self.__p
+                return (self(i+self.p)-self(i))/self.p
+
+Dfuns = {
+    "lReLU": difun(lambda x: max(x,x*.1)),
+    }
