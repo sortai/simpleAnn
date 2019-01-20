f60e70a6a7cf4b6e9fb48fb784a37b1b42477273
sortai <32301806+sortai@users.noreply.github.com>
2019-01-20 14:57:39 +0100

    Finished ann.__init__ and ann.__call__

--- ann.py
+++ ann.py
@@ -9,8 +9,10 @@ class layer:
 class ann (layer):
     def __init__(self, size, layers=None):
         if layers is None:
+            self._monolayer=True
             super().__init__(self, size)
         else:
+            self._monolayer=False
             if len(layers) < 1:
                 raise SizeMismatch("not enough layers provided")
             if size[0]!=layers[0].size[0]:
@@ -25,3 +27,12 @@ class ann (layer):
                     raise SizeMismatch("Layer output size of layer {ln0} ({so}) and layer input size of layer {ln1} ({si}) don't match".format(ln0=ln, ln1=ln+1, so=la.size[1], si=self.layers[ln+1].size[0]))
                 else: self.sizes.append(la.size[1])
             self.sizes.append(self.size[1])
+    @property
+    def monolayer(self):
+        return self._monolayer
+    def __call__(self, i):
+        if self._monolayer:
+            return super().__call__(self, i)
+        for layer in self.layers:
+            i=layer(i)
+        return i
