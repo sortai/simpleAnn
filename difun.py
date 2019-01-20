ffde0b5f872972edb0c2f5f1392edc89e6453414
sortai <32301806+sortai@users.noreply.github.com>
2019-01-20 16:47:35 +0100

    partially implemented difun

--- difun.py
+++ difun.py
@@ -2,6 +2,13 @@ import numpy as np
 
 class difun:
     def __init__(self, fun, funp=None, p=2**-10):
-        self._fun = fun
-        self._funp = funp
-        self._p = p
+        self.__fun = fun
+        self.__funp = funp
+        self.__p = p
+    def __call__(self, i):
+        return self.__fun(i)
+    def de(self, i):
+        try: return self.__funp(i)
+        except TypeError as error:
+            if error.args[0] == "'NoneType' object is not callable":
+                return (self(i+self.__p)-self(i))/self.__p
